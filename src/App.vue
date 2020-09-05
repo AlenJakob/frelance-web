8555a22b83703775abac84f1b55a3bf46354366c
Alen <33994452+AlenJakob@users.noreply.github.com>
2020-09-05 15:33:54 +0200

    installed bulma and setup

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/about">Our projects</router-link> |
-      <router-link to="/about">Contact</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
-
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+@import "~bulma/css/bulma.css";
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="nav" class="navbar">
+    <logo />
+
+    <div>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/about">Our projects</router-link>|
+      <router-link to="/about">Contact</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import logo from "./logo";
+export default {
+  components: {
+    logo
+  }
+};
+</script>
+
+<style>
+.navigation {
+  display: flex;
+}
+</style>
+
