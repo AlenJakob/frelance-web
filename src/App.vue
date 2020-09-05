f4ad207328ddcc538ddfb915404e3e9c7da392ed
Alen <33994452+AlenJakob@users.noreply.github.com>
2020-09-05 15:18:33 +0200

    first push

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/about">Our projects</router-link> |
+      <router-link to="/about">Contact</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -25,7 +29,7 @@
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #b94a42;
     }
   }
 }
