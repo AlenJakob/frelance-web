fd3afb124139a462089f7be7b0839452b60e1863
Alen <33994452+AlenJakob@users.noreply.github.com>
2020-09-13 21:31:01 +0200

    added style

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
-      <h1>Contact Page</h1>
-      <h2>Here will be a form to contact us and more details</h2>
+    <h1>Contact Page</h1>
+    <h2 class="subtitle">Here will be a form to contact us and more details</h2>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
