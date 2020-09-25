c4541ac3d36d6f76b9dbd25584a1153c10f5523a
Alen <33994452+AlenJakob@users.noreply.github.com>
2020-09-25 14:27:56 +0200

    creating card for project site

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="container columns">
-    <h1>Project Page</h1>
-    <CardProject class="is-4 column" />
+  <div class="container px-6">
+    <h1 class="title">Latest Projects</h1>
+    <div
+      class="columns is-multiline is-mobile"
+      v-for="project in projectList"
+      :key="project.id"
+    >
+      <CardProject class="" />
+    </div>
   </div>
 </template>
 
 <script>
+import projects from "../assets/db/projects.json";
 import CardProject from "../components/Projects/CardProject";
 export default {
   components: {
     CardProject
+  },
+  data() {
+    return {
+      projectList: projects
+    };
+  },
+  mounted() {
+    console.log(this.projectList);
   }
 };
 </script>
