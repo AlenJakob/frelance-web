855629b06c64ff14a5dbf03872a4dea029f85cbf
Alen <33994452+AlenJakob@users.noreply.github.com>
2020-09-29 19:50:35 +0200

    added 404 page graphic

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,16 @@
     <div class="content has-text-centered">
       <p>
         <strong class="has-text-danger">FreeBees</strong> Created by
-        <a class="has-text-danger" href="https://github.com/AlenJakob">Alen Jakob</a>
+        <a class="has-text-danger" href="https://github.com/AlenJakob"
+          >Alen Jakob</a
+        >
         <span> © 2020 All rights reserved</span>
       </p>
     </div>
   </footer>
 </template>
+<style>
+.footer {
+  align-self: flex-end!important;
+}
+</style>
